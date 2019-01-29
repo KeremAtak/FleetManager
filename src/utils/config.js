f4a84d5a6f1f0cbdab732b2980b1d41a2390ed50
KeremAtak <kerem.atak@hotmail.com>
2019-01-29 04:14:05 +0200

    tidying env, more eslint cleaning

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,20 +1,20 @@
 // Fetches configuration data from env to application
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
 let port = process.env.PORT
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'password',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'password',
+  port: 5432
 })
 
 module.exports = {
-    port, pool
+  port, pool
 }
